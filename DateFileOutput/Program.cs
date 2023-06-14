64a33e85a44ecef0d22787275d21214c4cdffa53
ihassan1 <ihassan1@kpmg.com.au>
2023-06-15 00:09:27 +1000

    initial commit

---
+++ DateFileOutput/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+string? DirectoryPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+string OutputPath = Path.Combine(DirectoryPath, "..", "..", "..", "txt_output", "output.txt");
+
+DateTime currentDateTime = DateTime.Now;
+string formattedDateTime = currentDateTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+Console.WriteLine(Path.GetFullPath(OutputPath));
+Console.WriteLine($"current time is {formattedDateTime}");
+
+using (StreamWriter writer = new StreamWriter(OutputPath))
+{
+    writer.WriteLine(formattedDateTime);
+}
